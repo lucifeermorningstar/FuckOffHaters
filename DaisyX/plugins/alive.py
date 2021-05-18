99400ca303fa3e3e2e6c8faf712375144ceec28c
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-18 18:43:51 +0530

    Update alive.py

--- DaisyX/plugins/alive.py
+++ DaisyX/plugins/alive.py
@@ -4,13 +4,13 @@ from datetime import datetime
 from pyrogram import filters
 from platform import python_version
 
-from DaisyX import SkemX, __version__
+from DaisyX import SkemX, __version__, StartTime
 
 @SkemX.on_message(filters.command("alive", ".") & filters.me)
 async def alive(app, message):
     txt = (
         f"**ᴅᴀɪsʏ χ** ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n"
-        f"==>> sᴛᴀʀᴛ ᴛɪᴍᴇ: `{str(datetime.now() - START_TIME).split('.')[0]}`\n"
+        f"==>> sᴛᴀʀᴛ ᴛɪᴍᴇ: `{str(datetime.now() - StartTime).split('.')[0]}`\n"
         f"==>> ᴅᴀɪsʏ χ ᴠᴇʀsɪᴏɴ: {__version__}\n"
         f"==>> ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ: `{python_version()}`\n"
         f"==>> ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ: `{pyrogram.__version__}`"
