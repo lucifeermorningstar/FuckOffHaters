dd16b4fbb8bc416a959d50895cde50ebf83a86f5
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-18 18:08:49 +0530

    Update alive.py

--- DaisyX/plugins/alive.py
+++ DaisyX/plugins/alive.py
@@ -1,8 +1,16 @@
-from pyrogram import filters
-from DaisyX import app
+# (c) Copyright 2021-2022 by lucifeermorningstar@GitHub , < https://github.com/lucifeermorningstar >
 
-@app.on_message(filters.regex("^.alive"))
-def amialivedad(event):
-    chat = event.chat.id 
-    message = " Master ! I am alive :)"
-    app.edit_message_text(chat_id=chat, message_id="me", text=message)
+from pyrogram import filters
+from DaisyX import app, __version__
+from platform import python_version
+
+@app.on_message(filters.command("alive", ".") & filters.me)
+async def alive(_, message: Message):
+    txt = (
+        f"**ᴅᴀɪsʏ χ** ɪs ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ\n"
+        f"==>> sᴛᴀʀᴛ ᴛɪᴍᴇ: `{str(datetime.now() - START_TIME).split('.')[0]}`\n"
+        f"==>> ᴅᴀɪsʏ χ ᴠᴇʀsɪᴏɴ: {__version__}\n"
+        f"==>> ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ: `{python_version()}`\n"
+        f"==>> ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ: `{pyrogram.__version__}`"
+    )
+    await message.edit(txt)
