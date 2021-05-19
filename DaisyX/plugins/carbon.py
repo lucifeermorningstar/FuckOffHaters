3de864c4352ae96d983875f4517a27b1af57c6f1
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-19 06:55:57 +0530

    Create carbon.py

---
+++ DaisyX/plugins/carbon.py
@@ -0,0 +1,21 @@
+import os
+
+from pyrogram import filters
+
+from DaisyX import app
+
+@app.on_message(filters.command("carbon"))
+@capture_err
+async def carbon_func(_, message):
+    if not message.reply_to_message:
+        await message.reply_text("Reply to a text message to make carbon.")
+        return
+    if not message.reply_to_message.text:
+        await message.reply_text("Reply to a text message to make carbon.")
+        return
+    m = await message.reply_text("Preparing Carbon")
+    carbon = await make_carbon(message.reply_to_message.text)
+    await m.edit("Uploading")
+    await app.send_photo(message.chat.id, carbon)
+    await m.delete()
+    os.remove(carbon)
