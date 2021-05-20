e8ae988091f05edaccce84ae6148506c7556dcac
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-20 17:56:44 +0530

    Update basic_helpers.py

--- DaisyX/functions/basic_helpers.py
+++ DaisyX/functions/basic_helpers.py
@@ -1,7 +1,5 @@
 
-from DaisyX import SUDO_USERS
-
-AFS = list(SUDO_USERS)
+from DaisyX import SUDO_USERS as AFS
 
 async def edit_or_reply(message, text, parse_mode="md"):
     """Edit Message If Its From Self, Else Reply To Message, (Only Works For Sudo's)"""
@@ -9,7 +7,7 @@ async def edit_or_reply(message, text, parse_mode="md"):
         return await message.edit(text, parse_mode=parse_mode)
     if not message.from_user:
         return await message.edit(text, parse_mode=parse_mode)
-    if message.from_user.id in Config.AFS:
+    if message.from_user.id in AFS:
         if message.reply_to_message:
             kk = message.reply_to_message.message_id
             return await message.reply_text(
