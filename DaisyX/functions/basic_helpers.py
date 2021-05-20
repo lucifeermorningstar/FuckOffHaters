936b286df8f1b4cb35b6af9c61ed852f6b631558
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-20 17:55:21 +0530

    Create basic_helpers.py

---
+++ DaisyX/functions/basic_helpers.py
@@ -0,0 +1,32 @@
+
+from DaisyX import SUDO_USERS
+
+AFS = list(SUDO_USERS)
+
+async def edit_or_reply(message, text, parse_mode="md"):
+    """Edit Message If Its From Self, Else Reply To Message, (Only Works For Sudo's)"""
+    if not message:
+        return await message.edit(text, parse_mode=parse_mode)
+    if not message.from_user:
+        return await message.edit(text, parse_mode=parse_mode)
+    if message.from_user.id in Config.AFS:
+        if message.reply_to_message:
+            kk = message.reply_to_message.message_id
+            return await message.reply_text(
+                text, reply_to_message_id=kk, parse_mode=parse_mode
+            )
+        return await message.reply_text(text, parse_mode=parse_mode)
+    return await message.edit(text, parse_mode=parse_mode)
+
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
