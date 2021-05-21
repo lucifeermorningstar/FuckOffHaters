20c09832404d0413b84df4ea0f8de07a911b8b2b
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-21 15:32:50 +0530

    Create restart.py

---
+++ DaisyX/plugins/restart.py
@@ -0,0 +1,11 @@
+# (c) Copyright 2021-2022 By lucifeermorningstar@GitHub , < https://github.com/lucifeermorningstar >
+
+import os, re, sys, asyncio
+from DaisyX import SkemX as app, command
+
+@app.on_message(command("restart") & filters.me) 
+async def restart(_, message):
+    m = await message.reply_text("Restarting") 
+    await m.edit("Restarted Successfully")    
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit ()
