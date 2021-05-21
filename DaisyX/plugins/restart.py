c144c0821ac992cda68006b22bc21385e450a3bd
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-21 15:57:12 +0530

    Update restart.py

--- DaisyX/plugins/restart.py
+++ DaisyX/plugins/restart.py
@@ -2,12 +2,23 @@
 
 import os, re, sys, asyncio
 from pyrogram import filters
+from pyrogram.types import Message
 
 from DaisyX import SkemX as app, command
 
-@app.on_message(command("restart") & filters.me) 
-async def restart(_, message):
-    m = await message.reply_text("Restarting") 
-    await m.edit("Restarted Successfully")    
-    os.execl(sys.executable, sys.executable, *sys.argv)
-    quit ()
+@app.on_message(command("restart") & filters.me)
+async def restart(_, message: Message):
+    await message.edit(f"Restarting {app.__class__.__name__}.")
+    await app.send_message(
+        "me", f"#userbot_restart, {message.chat.id}, {message.message_id}"
+    )
+
+    if "p" in message.text and "g" in message.text:
+        asyncio.get_event_loop().create_task(app.restart(git_update=True, pip=True))
+    elif "p" in message.text:
+        asyncio.get_event_loop().create_task(app.restart(pip=True))
+    elif "g" in message.text:
+        asyncio.get_event_loop().create_task(app.restart(git_update=True))
+    else:
+        asyncio.get_event_loop().create_task(app.restart())
+
