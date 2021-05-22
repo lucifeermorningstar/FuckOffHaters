84b7f0c7206b32128c5b4d692ce3b0049207ae45
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-22 09:18:46 +0530

    Update basic_helpers.py

--- DaisyX/functions/basic_helpers.py
+++ DaisyX/functions/basic_helpers.py
@@ -1,5 +1,4 @@
 import time
-from typing import Tuple
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait, MessageNotModified
 from PIL import Image
@@ -100,56 +99,3 @@ async def progress(current, total, message, start, type_of_ps, file_name=None):
             except MessageNotModified:
                 pass
 
-async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
-    """Run Commands"""
-    args = shlex.split(cmd)
-    process = await asyncio.create_subprocess_exec(
-        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    return (
-        stdout.decode("utf-8", "replace").strip(),
-        stderr.decode("utf-8", "replace").strip(),
-        process.returncode,
-        process.pid,
-    )
-
-async def convert_to_image(message, client) -> [None, str]:
-    """Convert Most Media Formats To Raw Image"""
-    if not message:
-        return None
-    if not message.reply_to_message:
-        return None
-    final_path = None
-    if not (
-        message.reply_to_message.video
-        or message.reply_to_message.photo
-        or message.reply_to_message.sticker
-        or message.reply_to_message.media
-        or message.reply_to_message.animation
-        or message.reply_to_message.audio
-    ):
-        return None
-    if message.reply_to_message.photo:
-        final_path = await message.reply_to_message.download()
-    elif message.reply_to_message.sticker:
-        if message.reply_to_message.sticker.mime_type == "image/webp":
-            final_path = "webp_to_png_s_proton.png"
-            path_s = await message.reply_to_message.download()
-            im = Image.open(path_s)
-            im.save(final_path, "PNG")
-        else:
-            path_s = await client.download_media(message.reply_to_message)
-            final_path = "lottie_proton.png"
-            cmd = (
-                f"lottie_convert.py --frame 0 -if lottie -of png {path_s} {final_path}"
-            )
-            await run_cmd(cmd)
-    elif message.reply_to_message.audio:
-        thumb = message.reply_to_message.audio.thumbs[0].file_id
-        final_path = await client.download_media(thumb)
-    elif message.reply_to_message.video or message.reply_to_message.animation:
-        final_path = "fetched_thumb.png"
-        vid_path = await client.download_media(message.reply_to_message)
-        await run_cmd(f"ffmpeg -i {vid_path} -filter:v scale=500:500 -an {final_path}")
-    return final_path
