fbdca3f5b7519a7a2d1fd3286e3749cf3b103a6c
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-23 14:16:25 +0530

    Update __init__.py

--- DaisyX/__init__.py
+++ DaisyX/__init__.py
@@ -17,7 +17,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 
 # Config in init as of now
 class Config(object):
-    API_ID = os.environ.get("API_ID", None) 
+    API_ID = os.environ.get("API_ID", 1) 
     API_HASH = os.environ.get("API_HASH", None) 
     SESSION = os.environ.get("SESSION", None) 
     PREFIX = os.environ.get("PREFIX", ".") # '.' as default prefix
