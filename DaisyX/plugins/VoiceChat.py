7688ed810e9f2e838ef3dbc204fffb0b9b0f9543
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-23 08:40:02 +0530

    Create VoiceChat.py

---
+++ DaisyX/plugins/VoiceChat.py
@@ -0,0 +1,54 @@
+import os
+
+import aiofiles
+import aiohttp
+from pyrogram import filters
+
+from DaisyX import SkemX as LYCIA, command
+
+
+async def fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            try:
+                data = await resp.json()
+            except:
+                data = await resp.text()
+    return data
+
+
+async def ai_lycia(url):
+    ai_name = "Daisyx.mp3"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(ai_name, mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+    return ai_name
+
+
+@LYCIA.on_message(command("Daisyx"))
+async def Lycia(_, message):
+    if len(message.command) < 2:
+        await message.reply_text("DaisyX AI Voice Chatbot")
+        return
+    text = message.text.split(None, 1)[1]
+    lycia = text.replace(" ", "%20")
+    m = await message.reply_text("Daisyx Is Best...")
+    try:
+        L = await fetch(
+            f"https://api.affiliateplus.xyz/api/chatbot?message={lycia}&botname=Daisy&ownername=TeamDaisyX&user=1"
+        )
+        chatbot = L["message"]
+        VoiceAi = f"https://lyciavoice.herokuapp.com/lycia?text={chatbot}&lang=hi"
+        name = "DaisyX"
+    except Exception as e:
+        await m.edit(str(e))
+        return
+    await m.edit("By @TeamDaisyX")
+    LyciaVoice = await ai_lycia(VoiceAi)
+    await m.edit("Repyping...")
+    await message.reply_audio(audio=LyciaVoice, title=chatbot, performer=name)
+    os.remove(LyciaVoice)
+    await m.delete()
