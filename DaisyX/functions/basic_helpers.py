b54956f3745d1a154f4e300d92627394e2431209
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-23 13:22:15 +0530

    Update basic_helpers.py

--- DaisyX/functions/basic_helpers.py
+++ DaisyX/functions/basic_helpers.py
@@ -1,9 +1,11 @@
 import time
+import os
 from pyrogram.types import Message
 from pyrogram.errors import FloodWait, MessageNotModified
 from PIL import Image
 
-from DaisyX import SUDO_USERS as AFS
+class Config(object):
+    AFS = os.environ.get("SUDO_USERS") 
 
 async def edit_or_reply(message, text, parse_mode="md"):
     """Edit Message If Its From Self, Else Reply To Message, (Only Works For Sudo's)"""
@@ -11,7 +13,7 @@ async def edit_or_reply(message, text, parse_mode="md"):
         return await message.edit(text, parse_mode=parse_mode)
     if not message.from_user:
         return await message.edit(text, parse_mode=parse_mode)
-    if message.from_user.id in AFS:
+    if message.from_user.id in Config.AFS:
         if message.reply_to_message:
             kk = message.reply_to_message.message_id
             return await message.reply_text(
