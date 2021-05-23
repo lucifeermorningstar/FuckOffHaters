1cbcff8116a4ebafd2ac396b714876c4c554ee3b
Devil <76868561+lucifeermorningstar@users.noreply.github.com>
2021-05-23 14:39:20 +0530

    Update heroku.py

--- DaisyX/plugins/heroku.py
+++ DaisyX/plugins/heroku.py
@@ -14,7 +14,7 @@ heroku_api = "https://api.heroku.com"
 HEROKU_APP_NAME = Config.HEROKU_APP_NAME
 HEROKU_API_KEY = Config.HEROKU_API_KEY
 
-Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
+Heroku = heroku3.from_key(Config.HEROKU_API_KEY)
 heroku_api = "https://api.heroku.com"
 
 
